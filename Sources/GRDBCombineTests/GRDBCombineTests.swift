6059f62bc9596b455c8aae2d227a828709ea227a
Gwendal Roué <gr@pierlis.com>
2019-06-10 20:56:03 +0200

    Test

--- Sources/GRDBCombineTests/GRDBCombineTests.swift
+++ Sources/GRDBCombineTests/GRDBCombineTests.swift
@@ -1,7 +1,25 @@
 import XCTest
+import GRDB
 import GRDBCombine
 
 class GRDBCombineTests: XCTest {
+    func test() throws {
+        struct Player: Codable, FetchableRecord, PersistableRecord {
+            var id: Int64
+            var name: String
+        }
+        let dbQueue = DatabaseQueue()
+        try dbQueue.write { db in
+            try db.create(table: "player") { t in
+                t.autoIncrementedPrimaryKey("id")
+                t.column("name", .text)
+            }
+        }
+        let players = Player.observationForAll()
+        let publisher = DatabasePublishers.Value(players, in: dbQueue)
+        let published1 = DatabasePublished(publisher)
+        let published2 = DatabasePublished(players, in: dbQueue)
+    }
     func testFailure() {
         XCTAssert(false, "failure")
     }
