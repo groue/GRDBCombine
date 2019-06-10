c7bf40ab9be0b24925f7d08ae70b9f37ee4cbc42
Gwendal Roué <gr@pierlis.com>
2019-06-10 08:51:15 +0200

    Attempt at fostering publishers over ValueObservation

--- Sources/GRDBCombine/GRDB+Combine.swift
+++ Sources/GRDBCombine/GRDB+Combine.swift
@@ -3,12 +3,22 @@ import GRDB
 // MARK: - Count
 
 extension FetchRequest {
+    public func count(in reader: DatabaseReader) -> DatabasePublishers.Value<ValueReducers.RemoveDuplicates<ValueReducers.Passthrough<Int>>> {
+        return DatabasePublishers.Value(ValueObservation.trackingCount(self), in: reader)
+    }
+    
+    @available(*, deprecated)
     public var observeCount: ValueObservation<ValueReducers.RemoveDuplicates<ValueReducers.Passthrough<Int>>> {
         return ValueObservation.trackingCount(self)
     }
 }
 
 extension TableRecord {
+    public static func count(in reader: DatabaseReader) -> DatabasePublishers.Value<ValueReducers.RemoveDuplicates<ValueReducers.Passthrough<Int>>> {
+        return all().count(in: reader)
+    }
+    
+    @available(*, deprecated)
     public static var observeCount: ValueObservation<ValueReducers.RemoveDuplicates<ValueReducers.Passthrough<Int>>> {
         return all().observeCount
     }
@@ -17,10 +27,20 @@ extension TableRecord {
 // MARK: - Row
 
 extension FetchRequest where RowDecoder == Row {
+    public func all(in reader: DatabaseReader) -> DatabasePublishers.Value<RowsReducer> {
+        return DatabasePublishers.Value(ValueObservation.trackingAll(self), in: reader)
+    }
+    
+    public func first(in reader: DatabaseReader) -> DatabasePublishers.Value<RowReducer> {
+        return DatabasePublishers.Value(ValueObservation.trackingOne(self), in: reader)
+    }
+    
+    @available(*, deprecated)
     public var observeAll: ValueObservation<RowsReducer> {
         return ValueObservation.trackingAll(self)
     }
     
+    @available(*, deprecated)
     public var observeOne: ValueObservation<RowReducer> {
         return ValueObservation.trackingOne(self)
     }
@@ -29,20 +49,40 @@ extension FetchRequest where RowDecoder == Row {
 // MARK: - FetchableRecord
 
 extension FetchRequest where RowDecoder: FetchableRecord {
+    public func all(in reader: DatabaseReader) -> DatabasePublishers.Value<FetchableRecordsReducer<RowDecoder>> {
+        return DatabasePublishers.Value(ValueObservation.trackingAll(self), in: reader)
+    }
+    
+    public func first(in reader: DatabaseReader) -> DatabasePublishers.Value<FetchableRecordReducer<RowDecoder>> {
+        return DatabasePublishers.Value(ValueObservation.trackingOne(self), in: reader)
+    }
+    
+    @available(*, deprecated)
     public var observeAll: ValueObservation<FetchableRecordsReducer<RowDecoder>> {
         return ValueObservation.trackingAll(self)
     }
     
+    @available(*, deprecated)
     public var observeOne: ValueObservation<FetchableRecordReducer<RowDecoder>> {
         return ValueObservation.trackingOne(self)
     }
 }
 
 extension TableRecord where Self: FetchableRecord {
+    public static func all(in reader: DatabaseReader) -> DatabasePublishers.Value<FetchableRecordsReducer<Self>> {
+        return all().all(in: reader)
+    }
+    
+    public static func first(in reader: DatabaseReader) -> DatabasePublishers.Value<FetchableRecordReducer<Self>> {
+        return limit(1).first(in: reader)
+    }
+    
+    @available(*, deprecated)
     public static var observeAll: ValueObservation<FetchableRecordsReducer<Self>> {
         return all().observeAll
     }
     
+    @available(*, deprecated)
     public static var observeOne: ValueObservation<FetchableRecordReducer<Self>> {
         return limit(1).observeOne
     }
@@ -51,16 +91,31 @@ extension TableRecord where Self: FetchableRecord {
 // MARK: - DatabaseValueConvertible
 
 extension FetchRequest where RowDecoder: DatabaseValueConvertible {
+    public func all(in reader: DatabaseReader) -> DatabasePublishers.Value<DatabaseValuesReducer<RowDecoder>> {
+        return DatabasePublishers.Value(ValueObservation.trackingAll(self), in: reader)
+    }
+    
+    public func first(in reader: DatabaseReader) -> DatabasePublishers.Value<DatabaseValueReducer<RowDecoder>> {
+        return DatabasePublishers.Value(ValueObservation.trackingOne(self), in: reader)
+    }
+    
+    @available(*, deprecated)
     public var observeAll: ValueObservation<DatabaseValuesReducer<RowDecoder>> {
         return ValueObservation.trackingAll(self)
     }
     
+    @available(*, deprecated)
     public var observeOne: ValueObservation<DatabaseValueReducer<RowDecoder>> {
         return ValueObservation.trackingOne(self)
     }
 }
 
 extension FetchRequest where RowDecoder: _OptionalProtocol, RowDecoder._Wrapped: DatabaseValueConvertible {
+    public func all(in reader: DatabaseReader) -> DatabasePublishers.Value<OptionalDatabaseValuesReducer<RowDecoder._Wrapped>> {
+        return DatabasePublishers.Value(ValueObservation.trackingAll(self), in: reader)
+    }
+    
+    @available(*, deprecated)
     public var observeAll: ValueObservation<OptionalDatabaseValuesReducer<RowDecoder._Wrapped>> {
         return ValueObservation.trackingAll(self)
     }
