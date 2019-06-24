c8b32b665ab7c296ef354ccd307b2fcc8af30944
Gwendal Roué <gr@pierlis.com>
2019-06-24 09:01:16 +0200

    More DatabasePublishedTests

--- Sources/GRDBCombineTests/DatabasePublishedTests.swift
+++ Sources/GRDBCombineTests/DatabasePublishedTests.swift
@@ -104,4 +104,118 @@ class DatabasePublishedTests : XCTestCase {
             .runInTemporaryDirectory("DatabasePool") { try prepare(DatabasePool(path: $0)) }
             .runInTemporaryDirectory("DatabaseSnapshot") { try prepare(DatabasePool(path: $0)).makeSnapshot() }
     }
+    
+    func testDatabasePublishedAsPublisher() throws {
+        func prepare<Writer: DatabaseWriter>(_ writer: Writer) throws -> Writer {
+            try writer.write { db in
+                try Player.createTable(db)
+            }
+            return writer
+        }
+        
+        func test(writer: DatabaseWriter, cancelBag: CancelBag, label: String) throws {
+            class Model {
+                static var countPublisher: DatabasePublishers.Value<Int>!
+                @DatabasePublished(countPublisher)
+                var count: Result<Int, Error>
+            }
+            
+            Model.countPublisher = Player.observationForCount().publisher(in: writer)
+            let model = Model()
+            
+            let expectation = self.expectation(description: label)
+            let testSubject = PassthroughSubject<Int, Error>()
+            testSubject
+                .collect(3)
+                .sink(
+                    receiveCompletion: { completion in
+                        XCTAssertNoFailure(completion, label: label)
+                },
+                    receiveValue: { value in
+                        XCTAssertEqual(value, [0, 1, 3])
+                        expectation.fulfill()
+                })
+                .add(to: cancelBag)
+            
+            model
+                .$count
+                .subscribe(testSubject)
+                .add(to: cancelBag)
+            
+            try writer.writeWithoutTransaction { db in
+                try Player(id: 1, name: "Arthur", score: 1000).insert(db)
+                
+                try db.inTransaction {
+                    try Player(id: 2, name: "Barbara", score: 750).insert(db)
+                    try Player(id: 3, name: "Craig", score: 500).insert(db)
+                    return .commit
+                }
+            }
+            
+            waitForExpectations(timeout: 1, handler: nil)
+        }
+        
+        try Test(test)
+            .run("InMemoryDatabaseQueue") { try prepare(DatabaseQueue()) }
+            .runInTemporaryDirectory("DatabaseQueue") { try prepare(DatabaseQueue(path: $0)) }
+            .runInTemporaryDirectory("DatabasePool") { try prepare(DatabasePool(path: $0)) }
+    }
+    
+    func testDatabasePublishedDidChange() throws {
+        func prepare<Writer: DatabaseWriter>(_ writer: Writer) throws -> Writer {
+            try writer.write { db in
+                try Player.createTable(db)
+            }
+            return writer
+        }
+        
+        func test(writer: DatabaseWriter, cancelBag: CancelBag, label: String) throws {
+            class Model {
+                static var countPublisher: DatabasePublishers.Value<Int>!
+                @DatabasePublished(countPublisher)
+                var count: Result<Int, Error>
+            }
+            
+            Model.countPublisher = Player.observationForCount().publisher(in: writer)
+            let model = Model()
+            
+            let expectation = self.expectation(description: label)
+            let testSubject = PassthroughSubject<Int, Error>()
+            testSubject
+                .collect(2)
+                .sink(
+                    receiveCompletion: { completion in
+                        XCTAssertNoFailure(completion, label: label)
+                },
+                    receiveValue: { value in
+                        XCTAssertEqual(value, [1, 3])
+                        expectation.fulfill()
+                })
+                .add(to: cancelBag)
+            
+            model
+                .$count
+                .didChange
+                .tryMap { try model.count.get() }
+                .subscribe(testSubject)
+                .add(to: cancelBag)
+            
+            try writer.writeWithoutTransaction { db in
+                try Player(id: 1, name: "Arthur", score: 1000).insert(db)
+                
+                try db.inTransaction {
+                    try Player(id: 2, name: "Barbara", score: 750).insert(db)
+                    try Player(id: 3, name: "Craig", score: 500).insert(db)
+                    return .commit
+                }
+            }
+            
+            waitForExpectations(timeout: 1, handler: nil)
+        }
+        
+        try Test(test)
+            .run("InMemoryDatabaseQueue") { try prepare(DatabaseQueue()) }
+            .runInTemporaryDirectory("DatabaseQueue") { try prepare(DatabaseQueue(path: $0)) }
+            .runInTemporaryDirectory("DatabasePool") { try prepare(DatabasePool(path: $0)) }
+    }
 }
