98445e67b54b4325613d5ec5623bce5eb6a06d19
Gwendal Roué <gr@pierlis.com>
2019-07-26 04:47:53 +0200

    TODO

--- Documentation/Demo/GRDBCombineDemo/Combine+App.swift
+++ Documentation/Demo/GRDBCombineDemo/Combine+App.swift
@@ -1,6 +1,7 @@
 import Combine
 
 extension Publisher where Failure == Never {
+    // TODO: remove when Combine supports ships with this operator.
     /// Support for publishers of non-optionals so that they can feed keypaths
     /// to optionals. This API is likely to be eventually added to Combine.
     func assign<Root>(to keyPath: ReferenceWritableKeyPath<Root, Output?>, on object: Root) -> AnyCancellable {
